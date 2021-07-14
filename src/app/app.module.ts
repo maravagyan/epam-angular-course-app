1d8fcf1341fa00640786b22db49c922db2756609
maravagyan <mariamavagyan7@gmail.com>
2021-07-14 23:38:56 +0400

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoCardComponent
+    
   ],
   imports: [
     BrowserModule
